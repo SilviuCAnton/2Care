bfe6ac3cbb021f4a38923095c89668e4409d87a3
SilviuCAnton <silviu.c.anton@gmail.com>
2019-11-23 23:33:56 +0200

    test repo db - working

--- 2Care_Server/src/main/java/MainServer.java
+++ 2Care_Server/src/main/java/MainServer.java
@@ -1,5 +1,8 @@
 
 
+import domain.UserAccount;
+import repositories.UserAccountDBRepository;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -7,6 +10,7 @@ import java.net.Socket;
 public class MainServer {
     public static void main(String[] args) throws IOException {
         ServerSocket server = null;
+        UserAccountDBRepository userAccountDBRepository = new UserAccountDBRepository("jdbc:postgresql://localhost:5432/2CareDB", "postgres", "Super_paSS");
         try {
             server = new ServerSocket(1256);
             while(true) {
